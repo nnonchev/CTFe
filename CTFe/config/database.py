915de973be9941376026b9cfde8e1739547c576f
nnonchev <nnonchev@protonmail.com>
2020-09-13 20:56:45 +0200

    fix missing constants module

--- CTFe/config/database.py
+++ CTFe/config/database.py
@@ -1,4 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+
+from CTFe.config import constants
 
 
 # The model's base class configuration
