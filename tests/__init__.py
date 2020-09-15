0cf89a845ab671a9e958f9d885f0e4428b1c3aef
nnonchev <nnonchev@protonmail.com>
2020-09-15 06:40:56 +0200

    user created in test db

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,8 +2,24 @@ from fastapi.testclient import TestClient
 
 from CTFe.main import app
 from CTFe.config import constants
-from CTFe.config.database import dal
+from CTFe.models import *
+from CTFe.config.database import (
+    dal,
+    Base,
+)
 
 dal.db_url = f"{constants.DB_TYPE}://{constants.TEST_DB_USERNAME}:{constants.TEST_DB_PASSWORD}@{constants.TEST_DB_ADDRESS}:{constants.TEST_DB_PORT}/{constants.TEST_DB_NAME}"
+
+dal.init()
+
+Base.metadata.drop_all(dal.engine)
+Base.metadata.create_all(dal.engine)
+
+# Create user
+with dal.get_session_ctx() as session:
+    user = User(username="client1", password="secret")
+
+    session.add(user)
+    session.commit()
 
 client = TestClient(app)
