152da2b078d31e1f006fe0192230560b4b89905b
nnonchev <nnonchev@protonmail.com>
2020-09-15 06:40:09 +0200

    added basic user schema

---
+++ CTFe/schemas/user_schemas.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+
+class UserDetails(BaseModel):
+    id: int
+    username: str
+
+    class Config:
+        orm_mode = True
