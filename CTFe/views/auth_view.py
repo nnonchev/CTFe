370f6327856457648f6cb945689ccb1c44fe58e8
nnonchev <nnonchev@protonmail.com>
2020-09-15 13:51:53 +0200

    added logout view

--- CTFe/views/auth_view.py
+++ CTFe/views/auth_view.py
@@ -1,9 +1,12 @@
+from typing import Optional
+
 from sqlalchemy import and_
 from sqlalchemy.orm import Session
 from fastapi import (
     APIRouter,
     Depends,
     Response,
+    Cookie,
     status,
     HTTPException,
 )
@@ -23,7 +26,7 @@ router = APIRouter()
     "/login",
     response_model=user_schemas.UserDetails,
 )
-async def login_player(
+async def login_user(
     *,
     user_login: user_schemas.UserLogin,
     session: Session = Depends(dal.get_session),
@@ -45,3 +48,24 @@ async def login_player(
     response.set_cookie(key="token", value=token)
 
     return db_user
+
+
+@router.post(
+    "/logout",
+    status_code=204,
+)
+async def logout_user(
+    *,
+    token: Optional[str] = Cookie(None),
+    response: Response,
+):
+    if token is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="You are not logged in",
+        )
+
+    user_payload = await redis_utils.retrieve_payload(token, redis_dal)
+    await redis_utils.delete_key(user_payload.id, redis_dal)
+
+    response.delete_cookie("token")
