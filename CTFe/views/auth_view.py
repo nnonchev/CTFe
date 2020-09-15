b83cde030d1e68e463280172cf0fe94f4ac10467
nnonchev <nnonchev@protonmail.com>
2020-09-15 16:04:35 +0200

    added logged in user info view and register view

--- CTFe/views/auth_view.py
+++ CTFe/views/auth_view.py
@@ -22,6 +22,70 @@ from CTFe.utils.redis_utils import redis_dal
 router = APIRouter()
 
 
+@router.get(
+    "/me",
+    response_model=user_schemas.UserDetails,
+)
+async def user_info(
+    *,
+    token: Optional[str] = Cookie(None),
+    session: Session = Depends(dal.get_session),
+) -> user_schemas.UserDetails:
+    """ Get information on the currently logged in user """
+    if token is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="You are not logged in",
+        )
+
+    user_payload = await redis_utils.retrieve_payload(token, redis_dal)
+
+    conditions = and_(
+        User.id == user_payload.id,
+    )
+
+    db_user = user_ops.read_user_by_(session, conditions)
+
+    if db_user is None:
+        raise HTTPException(
+            status_code=404,
+            detail="User not found"
+        )
+
+    return db_user
+
+
+@router.post(
+    "/register",
+    response_model=user_schemas.UserDetails,
+)
+async def register_user(
+    *,
+    user_create: user_schemas.UserCreate,
+    session: Session = Depends(dal.get_session),
+    response: Response,
+):
+    """ Check if user with username already exists """
+    conditions = and_(
+        User.username == user_create.username,
+    )
+
+    if user_ops.read_user_by_(session, conditions):
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"The username: { user_create.username } is already taken"
+        )
+
+    db_user = user_ops.create_user(session, user_create)
+
+    user_payload = user_schemas.UserRedisPayload.from_orm(db_user)
+    token = await redis_utils.store_payload(user_payload, redis_dal)
+
+    response.set_cookie(key="token", value=token)
+
+    return db_user
+
+
 @router.post(
     "/login",
     response_model=user_schemas.UserDetails,
@@ -40,7 +104,10 @@ async def login_user(
     db_user = user_ops.read_user_by_(session, conditions)
 
     if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(
+            status_code=404,
+            detail="User not found"
+        )
 
     user_payload = user_schemas.UserRedisPayload.from_orm(db_user)
     token = await redis_utils.store_payload(user_payload, redis_dal)
