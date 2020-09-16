8df7af3c925ff2690af51d2c7c73a196762c5a50
nnonchev <nnonchev@protonmail.com>
2020-09-16 06:21:17 +0200

    added user_type column field

--- CTFe/models/user_model.py
+++ CTFe/models/user_model.py
@@ -1,6 +1,7 @@
 import sqlalchemy as sa
 
 from CTFe.config.database import Base
+from CTFe.utils.enums import UserType
 
 
 class User(Base):
@@ -19,3 +20,15 @@ class User(Base):
         sa.String(),
         nullable=True,
     )
+    user_type = sa.Column(
+        sa.Enum(UserType),
+        nullable=False,
+        server_default=UserType.PLAYER.name,
+    )
+
+    def __repr__(self):
+        return f"<User { self.id }>"
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
