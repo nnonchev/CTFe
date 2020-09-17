5a4e4b80090f5aa3c02e4131e51336ae516abf0b
nnonchev <nnonchev@protonmail.com>
2020-09-17 13:14:11 +0200

    added tests for user_view

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,4 +1,5 @@
-from fastapi.testclient import TestClient
+import pytest
+from httpx import AsyncClient
 
 from CTFe.main import app
 from CTFe.models import User
@@ -6,57 +7,280 @@ from CTFe.schemas import user_schemas
 from CTFe.utils import (
     enums,
     redis_utils,
+    validators,
 )
-from . import dal
+from . import (
+    dal,
+    BASE_URL,
+)
 
 
-def test_create_user__no_token():
-    client = TestClient(app)
+@pytest.mark.asyncio
+async def test_create_user__already_exists():
+    app.dependency_overrides[validators.validate_admin] = lambda: None
 
-    json_data = {
+    user_data = {
         "username": "user1",
         "password": "secret",
     }
 
-    response = client.post("/users/", json=json_data)
+    db_user = User(**user_data)
 
-    assert response.status_code == 401
-    assert response.json() == {"detail": "You are not logged in"}
+    with dal.get_session_ctx() as session:
+        session.add(db_user)
+        session.commit()
+        session.refresh(db_user)
+
+    async with AsyncClient(app=app, base_url=BASE_URL) as client:
+        response = await client.post("/users/", json=user_data)
+
+    assert response.status_code == 409
+    assert response.json() == {
+        "detail": f"The username: { user_data['username'] } is already taken"}
+
+    with dal.get_session_ctx() as session:
+        session.delete(db_user)
+        session.commit()
+
+    app.dependency_overrides = {}
 
 
-async def test_create_user__incorrect_user_type():
-    client = TestClient(app)
+@pytest.mark.asyncio
+async def test_create_user__success():
+    app.dependency_overrides[validators.validate_admin] = lambda: None
 
-    json_data = {
-        "username": "player1",
+    user_data = {
+        "username": "user1",
         "password": "secret",
     }
 
-    db_admin = User(username="admin1", password="secret")
-    db_admin.user_type = enums.UserType.ADMIN
+    async with AsyncClient(app=app, base_url=BASE_URL) as client:
+        response = await client.post("/users/", json=user_data)
 
-    db_player = User(username="player1", password="secret")
+    assert response.status_code == 200
+    assert "id" in response.json().keys()
+    assert ("username", user_data["username"]) in response.json().items()
 
     with dal.get_session_ctx() as session:
-        session.add(db_admin)
-        session.add(db_player)
+        id = response.json()["id"]
+        db_user = session.query(User).filter(User.id == id).first()
 
+        session.delete(db_user)
         session.commit()
 
-        session.refresh(db_admin)
-        session.refresh(db_player)
+    app.dependency_overrides = {}
 
-    user_payload = user_schemas.UserRedisPayload.from_orm(db_player)
-    token = await redis_utils.store_payload(user_payload, redis_dal)
 
-    client.cookies["token"] = token
+@pytest.mark.asyncio
+async def test_get_user__not_found():
+    app.dependency_overrides[validators.validate_admin] = lambda: None
 
-    response = client.post("/users/", json=json_data)
+    async with AsyncClient(app=app, base_url=BASE_URL) as client:
+        response = await client.get("/users/-1")
+
+    assert response.status_code == 404
+    assert response.json() == {"detail": "User not found"}
+
+    app.dependency_overrides = {}
+
+
+@pytest.mark.asyncio
+async def test_get_user__success():
+    app.dependency_overrides[validators.validate_admin] = lambda: None
+
+    user_data = {
+        "username": "user1",
+        "password": "secret",
+    }
+
+    db_user = User(**user_data)
+
+    with dal.get_session_ctx() as session:
+        session.add(db_user)
+        session.commit()
+        session.refresh(db_user)
+
+    async with AsyncClient(app=app, base_url=BASE_URL) as client:
+        response = await client.get(f"/users/{db_user.id}")
+
+    assert response.status_code == 200
+    assert response.json() == user_schemas.UserDetails.from_orm(db_user)
+
+    with dal.get_session_ctx() as session:
+        session.delete(db_user)
+        session.commit()
+
+    app.dependency_overrides = {}
+
+
+@pytest.mark.asyncio
+async def test_get_user_by_username__not_found():
+    app.dependency_overrides[validators.validate_admin] = lambda: None
+
+    username = "user1"
+
+    async with AsyncClient(app=app, base_url=BASE_URL) as client:
+        response = await client.get(f"/users/username/{username}")
+
+    assert response.status_code == 404
+    assert response.json() == {"detail": "User not found"}
+
+    app.dependency_overrides = {}
+
+
+@pytest.mark.asyncio
+async def test_get_user__success():
+    app.dependency_overrides[validators.validate_admin] = lambda: None
+
+    user_data = {
+        "username": "user1",
+        "password": "secret",
+    }
+
+    db_user = User(**user_data)
+
+    with dal.get_session_ctx() as session:
+        session.add(db_user)
+        session.commit()
+        session.refresh(db_user)
+
+    async with AsyncClient(app=app, base_url=BASE_URL) as client:
+        response = await client.get(f"/users/username/{db_user.username}")
+
+    assert response.status_code == 200
+    assert response.json() == user_schemas.UserDetails.from_orm(db_user)
+
+    with dal.get_session_ctx() as session:
+        session.delete(db_user)
+        session.commit()
+
+    app.dependency_overrides = {}
+
+
+@pytest.mark.asyncio
+async def test_get_users__success():
+    app.dependency_overrides[validators.validate_admin] = lambda: None
+
+    async with AsyncClient(app=app, base_url=BASE_URL) as client:
+        response = await client.get("/users/")
+
+    assert response.status_code == 200
+    assert response.json() == []
+
+    app.dependency_overrides = {}
+
+
+@pytest.mark.asyncio
+async def test_update_user__wrong_user_type():
+    app.dependency_overrides[validators.validate_admin] = lambda: None
+
+    user_data = {
+        "username": "user1",
+        "password": "secret",
+    }
+
+    new_user_data = {
+        "user_type": "None"
+    }
+
+    db_user = User(**user_data)
+
+    with dal.get_session_ctx() as session:
+        session.add(db_user)
+        session.commit()
+        session.refresh(db_user)
+
+    async with AsyncClient(app=app, base_url=BASE_URL) as client:
+        response = await client.put(f"/users/{db_user.id}", json=new_user_data)
+
+    with dal.get_session_ctx() as session:
+        session.add(db_user)
+        session.refresh(db_user)
+
     assert response.status_code == 401
-    assert response.json() == {"detail": "You don't have the correct access"}
+    assert response.json() == {"detail": "Wrong user_type value"}
 
     with dal.get_session_ctx() as session:
-        session.delete(db_admin)
-        session.delete(db_player)
-
+        session.delete(db_user)
         session.commit()
+
+    app.dependency_overrides = {}
+
+
+@pytest.mark.asyncio
+async def test_update_user__success():
+    app.dependency_overrides[validators.validate_admin] = lambda: None
+
+    user_data = {
+        "username": "user1",
+        "password": "secret",
+    }
+
+    new_user_data = {
+        "user_type": "admin",
+        "password": "better_secret",
+    }
+
+    db_user = User(**user_data)
+
+    with dal.get_session_ctx() as session:
+        session.add(db_user)
+        session.commit()
+        session.refresh(db_user)
+
+    async with AsyncClient(app=app, base_url=BASE_URL) as client:
+        response = await client.put(f"/users/{db_user.id}", json=new_user_data)
+
+    with dal.get_session_ctx() as session:
+        session.add(db_user)
+        session.refresh(db_user)
+
+    assert response.status_code == 200
+    assert response.json() == user_schemas.UserDetails.from_orm(db_user)
+
+    with dal.get_session_ctx() as session:
+        session.delete(db_user)
+        session.commit()
+
+    app.dependency_overrides = {}
+
+
+@pytest.mark.asyncio
+async def test_delete_user__not_found():
+    app.dependency_overrides[validators.validate_admin] = lambda: None
+
+    async with AsyncClient(app=app, base_url=BASE_URL) as client:
+        response = await client.delete(f"/users/-1")
+
+    assert response.status_code == 404
+    assert response.json() == {"detail": "User not found"}
+
+    app.dependency_overrides = {}
+
+
+@pytest.mark.asyncio
+async def test_delete_user__success():
+    app.dependency_overrides[validators.validate_admin] = lambda: None
+
+    user_data = {
+        "username": "user1",
+        "password": "secret",
+    }
+
+    db_user = User(**user_data)
+
+    with dal.get_session_ctx() as session:
+        session.add(db_user)
+        session.commit()
+        session.refresh(db_user)
+
+    async with AsyncClient(app=app, base_url=BASE_URL) as client:
+        response = await client.delete(f"/users/{db_user.id}")
+
+    assert response.status_code == 204
+
+    with dal.get_session_ctx() as session:
+        session.delete(db_user)
+        session.commit()
+
+    app.dependency_overrides = {}
