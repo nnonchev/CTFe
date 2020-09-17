86f8706a4f6ef16a05ff761824b0af2c050a6d10
nnonchev <nnonchev@protonmail.com>
2020-09-17 13:13:37 +0200

    added read, update, and delete views

--- CTFe/views/user_view.py
+++ CTFe/views/user_view.py
@@ -1,5 +1,9 @@
-from typing import Optional
+from typing import (
+    List,
+    Optional,
+)
 
+from sqlalchemy import and_
 from sqlalchemy.orm import Session
 from fastapi import (
     APIRouter,
@@ -10,20 +14,132 @@ from fastapi import (
 )
 
 from CTFe.config.database import dal
+from CTFe.models import User
 from CTFe.schemas import user_schemas
 from CTFe.operations import user_ops
+from CTFe.utils import enums
 
 
 router = APIRouter()
 
 
 @router.post("/", response_model=user_schemas.UserDetails)
-def create_user(
+async def create_user(
     *,
     user_create: user_schemas.UserCreate,
     session: Session = Depends(dal.get_session),
 ) -> user_schemas.UserDetails:
-    """ Create new user and save it into DB """
+    """ Create new user DB record """
+    conditions = and_(
+        User.username == user_create.username,
+    )
+
+    # Check if a user record with the same unique fields already exists
+    if user_ops.read_user_by_(session, conditions):
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"The username: { user_create.username } is already taken"
+        )
+
     db_user = user_ops.create_user(session, user_create)
 
     return db_user
+
+
+@router.get("/{id}", response_model=user_schemas.UserDetails)
+async def get_user(
+    *,
+    id: int,
+    session: Session = Depends(dal.get_session)
+) -> user_schemas.UserDetails:
+    """ Retrieve a single user from DB """
+    conditions = and_(
+        User.id == id,
+    )
+
+    db_user = user_ops.read_user_by_(session, conditions)
+
+    if db_user is None:
+        raise HTTPException(
+            status_code=404,
+            detail="User not found"
+        )
+
+    return db_user
+
+
+@router.get("/username/{username}", response_model=user_schemas.UserDetails)
+async def get_user_by_username(
+    *,
+    username: str,
+    session: Session = Depends(dal.get_session)
+) -> user_schemas.UserDetails:
+    """ Retrieve a single user from DB """
+    conditions = and_(
+        User.username == username,
+    )
+
+    db_user = user_ops.read_user_by_(session, conditions)
+
+    if db_user is None:
+        raise HTTPException(
+            status_code=404,
+            detail="User not found"
+        )
+
+    return db_user
+
+
+@router.get("/", response_model=List[user_schemas.UserDetails])
+async def get_all_users(
+    session: Session = Depends(dal.get_session)
+) -> List[user_schemas.UserDetails]:
+    """ Retreive all users from DB """
+    db_users = user_ops.read_users_by_(session, and_())
+    return db_users.all()
+
+
+@router.put("/{id}", response_model=user_schemas.UserDetails)
+async def update_user(
+    *,
+    id: int,
+    user_update: user_schemas.UserUpdate,
+    session: Session = Depends(dal.get_session)
+) -> user_schemas.UserDetails:
+    """ Update user from DB """
+    if user_update.user_type is not None and not enums.UserType.has_type(user_update.user_type):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"Wrong user_type value"
+        )
+
+    conditions = and_(
+        User.id == id,
+    )
+
+    db_user = user_ops.read_user_by_(session, conditions)
+
+    db_user = user_ops.update_user(session, db_user, user_update)
+    return db_user
+
+
+@router.delete("/{id}", status_code=204)
+def delete_user(
+    *,
+    id: int,
+    session: Session = Depends(dal.get_session)
+):
+    conditions = and_(
+        User.id == id,
+    )
+
+    db_user = user_ops.read_user_by_(session, conditions)
+
+    if db_user is None:
+        raise HTTPException(
+            status_code=404,
+            detail="User not found"
+        )
+
+    """ Delete user record from DB """
+    user_ops.delete_user(session, db_user)
