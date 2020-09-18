098afecd75212196e535957ab310fb6c32371574
nnonchev <nnonchev@protonmail.com>
2020-09-18 10:21:56 +0200

    added challenge schemas

---
+++ CTFe/schemas/challenge_schemas.py
@@ -0,0 +1,32 @@
+from typing import (
+    List,
+    Optional,
+)
+
+from pydantic import BaseModel
+
+
+class ChallengeCreate(BaseModel):
+    name: str
+    description: Optional[str] = None
+    flag: str
+    file_name: Optional[str] = None
+
+
+class ChallengeUpdate(BaseModel):
+    name: Optional[str] = None
+    description: Optional[str] = None
+    flag: Optional[str] = None
+    file_name: Optional[str] = None
+
+    class Config:
+        orm_mode = True
+
+
+class ChallengeDetails(BaseModel):
+    id: int
+    name: str
+    flag: str
+
+    class Config:
+        orm_mode = True
