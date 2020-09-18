3f3d970fcc2016a31bf40b244636ff75375e8291
nnonchev <nnonchev@protonmail.com>
2020-09-18 10:22:06 +0200

    added challenge views

--- CTFe/views/__init__.py
+++ CTFe/views/__init__.py
@@ -1,3 +1,4 @@
 from CTFe.views.auth_view import router as auth_router
 from CTFe.views.user_view import router as user_router
 from CTFe.views.team_view import router as team_router
+from CTFe.views.challenge_view import router as challenge_router
---
+++ CTFe/views/challenge_view.py
@@ -0,0 +1,146 @@
+from typing import List
+
+from sqlalchemy import and_
+from sqlalchemy.orm import Session
+from fastapi import (
+    APIRouter,
+    Depends,
+    Cookie,
+    status,
+    HTTPException,
+)
+
+from CTFe.config.database import dal
+from CTFe.models import Challenge
+from CTFe.schemas import challenge_schemas
+from CTFe.operations import challenge_ops
+
+
+router = APIRouter()
+
+
+@router.post("/", response_model=challenge_schemas.ChallengeDetails)
+async def create_challenge(
+    *,
+    challenge_create: challenge_schemas.ChallengeCreate,
+    session: Session = Depends(dal.get_session),
+) -> challenge_schemas.ChallengeDetails:
+    """ Create new challenge DB record """
+    conditions = and_(
+        Challenge.name == challenge_create.name,
+    )
+
+    # Check if a challenge record with the same unique fields already exists
+    if challenge_ops.read_challenges_by_(session, conditions).first():
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"The name: { challenge_create.name } is already taken"
+        )
+
+    db_challenge = challenge_ops.create_challenge(session, challenge_create)
+
+    return db_challenge
+
+
+@router.get("/{id}", response_model=challenge_schemas.ChallengeDetails)
+async def get_challenge(
+    *,
+    id: int,
+    session: Session = Depends(dal.get_session)
+) -> challenge_schemas.ChallengeDetails:
+    """ Retrieve a challenge record from DB """
+    conditions = and_(
+        Challenge.id == id,
+    )
+
+    db_challenge = challenge_ops.read_challenges_by_(
+        session, conditions).first()
+
+    if db_challenge is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Challenge not found"
+        )
+
+    return db_challenge
+
+
+@router.get("/name/{name}", response_model=challenge_schemas.ChallengeDetails)
+async def get_challenge_by_name(
+    *,
+    name: str,
+    session: Session = Depends(dal.get_session)
+) -> challenge_schemas.ChallengeDetails:
+    """ Retrieve a challenge record from DB """
+    conditions = and_(
+        Challenge.name == name,
+    )
+
+    db_challenge = challenge_ops.read_challenges_by_(
+        session, conditions).first()
+
+    if db_challenge is None:
+        raise HTTPException(
+            status.HTTP_404_NOT_FOUND,
+            detail="Challenge not found"
+        )
+
+    return db_challenge
+
+
+@router.get("/", response_model=List[challenge_schemas.ChallengeDetails])
+async def get_all_challenges(
+    session: Session = Depends(dal.get_session)
+) -> List[challenge_schemas.ChallengeDetails]:
+    """ Retreive multiple challenge records from DB """
+    db_challenges = challenge_ops.read_challenges_by_(session, and_())
+    return db_challenges.all()
+
+
+@router.put("/{id}", response_model=challenge_schemas.ChallengeDetails)
+async def update_challenge(
+    *,
+    id: int,
+    challenge_update: challenge_schemas.ChallengeUpdate,
+    session: Session = Depends(dal.get_session)
+) -> challenge_schemas.ChallengeDetails:
+    """ Update a challenge record from DB """
+    conditions = and_(
+        Challenge.id == id,
+    )
+
+    db_challenge = challenge_ops.read_challenges_by_(
+        session, conditions).first()
+
+    if db_challenge is None:
+        raise HTTPException(
+            status.HTTP_404_NOT_FOUND,
+            detail="Challenge not found"
+        )
+
+    db_challenge = challenge_ops.update_challenge(
+        session, db_challenge, challenge_update)
+    return db_challenge
+
+
+@router.delete("/{id}", status_code=204)
+def delete_user(
+    *,
+    id: int,
+    session: Session = Depends(dal.get_session)
+):
+    """ Delete a challenge record from DB """
+    conditions = and_(
+        Challenge.id == id,
+    )
+
+    db_challenge = challenge_ops.read_challenges_by_(
+        session, conditions).first()
+
+    if db_challenge is None:
+        raise HTTPException(
+            status.HTTP_404_NOT_FOUND,
+            detail="Challenge not found"
+        )
+
+    challenge_ops.delete_challenge(session, db_challenge)
