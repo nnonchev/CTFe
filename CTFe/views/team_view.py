5880315e30ca241bf0456982aef0b65da9ac10b2
nnonchev <nnonchev@protonmail.com>
2020-09-18 08:41:14 +0200

    added add_player and remove_player views

--- CTFe/views/team_view.py
+++ CTFe/views/team_view.py
@@ -42,7 +42,7 @@ async def create_team(
     )
 
     # Check if a team record with the same unique fields already exists
-    if team_ops.read_team_by_(session, conditions):
+    if team_ops.read_team_by_(session, conditions).first():
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
             detail=f"The name: { team_create.name } is already taken"
@@ -64,11 +64,11 @@ async def get_team(
         Team.id == id,
     )
 
-    db_team = team_ops.read_team_by_(session, conditions)
+    db_team = team_ops.read_team_by_(session, conditions).first()
 
     if db_team is None:
         raise HTTPException(
-            status_code=404,
+            status_code=status.HTTP_404_NOT_FOUND,
             detail="Team not found"
         )
 
@@ -86,11 +86,11 @@ async def get_team_by_name(
         Team.name == name,
     )
 
-    db_team = team_ops.read_team_by_(session, conditions)
+    db_team = team_ops.read_team_by_(session, conditions).first()
 
     if db_team is None:
         raise HTTPException(
-            status_code=404,
+            status.HTTP_404_NOT_FOUND,
             detail="Team not found"
         )
 
@@ -118,15 +118,119 @@ async def update_team(
         Team.id == id,
     )
 
-    db_team = team_ops.read_team_by_(session, conditions)
+    db_team = team_ops.read_team_by_(session, conditions).first()
 
     if db_team is None:
         raise HTTPException(
-            status_code=404,
+            status.HTTP_404_NOT_FOUND,
             detail="Team not found"
         )
 
     db_team = team_ops.update_team(session, db_team, team_update)
+    return db_team
+
+
+@router.patch("/{id}/add-player/{player_id}", response_model=team_schemas.TeamDetails)
+def add_player(
+    *,
+    id: int,
+    player_id: int,
+    session: Session = Depends(dal.get_session)
+) -> team_schemas.TeamDetails:
+    get_team_conditions = and_(
+        Team.id == id,
+    )
+
+    db_team = team_ops.read_team_by_(session, get_team_conditions).first()
+
+    # Check if team exists
+    if db_team is None:
+        raise HTTPException(
+            status.HTTP_404_NOT_FOUND,
+            detail="Team not found"
+        )
+
+    # Check if the already added players don't exceed the max team members constnat
+    if len(db_team.players) >= constants.MAX_TEAM_MEMBERS:
+        raise HTTPException(
+            status.HTTP_403_FORBIDDEN,
+            detail="The team already has the maximum number of members in it"
+        )
+
+    get_player_conditions = and_(
+        User.id == player_id
+    )
+
+    db_player = user_ops.read_user_by_(session, get_player_conditions).first()
+
+    # Check if user exists
+    if db_player is None:
+        raise HTTPException(
+            status.HTTP_404_NOT_FOUND,
+            detail="Player not found"
+        )
+
+    # Check if user is of user_type player
+    if db_player.user_type != enums.UserType.PLAYER:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail=f"Only players can be part of teams. { db_player } has user_type: { db_player.user_type }",
+        )
+
+    # Check if user hasn't already joined another team
+    if db_player.team is not None:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail=f"Player { db_player } is already part of a team",
+        )
+
+    db_team = team_ops.add_player(session, db_team, db_player)
+
+    return db_team
+
+
+@router.patch("/{id}/remove-player/{player_id}", response_model=team_schemas.TeamDetails)
+def remove_player(
+    *,
+    id: int,
+    player_id: int,
+    session: Session = Depends(dal.get_session)
+) -> team_schemas.TeamDetails:
+    get_team_conditions = and_(
+        Team.id == id,
+    )
+
+    db_team = team_ops.read_team_by_(session, get_team_conditions).first()
+
+    # Check if team exists
+    if db_team is None:
+        raise HTTPException(
+            status.HTTP_404_NOT_FOUND,
+            detail="Team not found"
+        )
+
+    get_player_conditions = and_(
+        User.id == player_id
+    )
+
+    db_player = user_ops.read_user_by_(session, get_player_conditions).first()
+
+    # Check if user exists
+    if db_player is None:
+        raise HTTPException(
+            status.HTTP_404_NOT_FOUND,
+            detail="Player not found"
+        )
+
+    # Check if user is part of the current team
+    if db_player.team_id != db_team.id:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail=f"Player { db_player } is not part of this team",
+        )
+
+    db_team = team_ops.remove_player(session, db_team, db_player)
+
     return db_team
 
 
@@ -141,11 +245,11 @@ def delete_user(
         Team.id == id,
     )
 
-    db_team = team_ops.read_team_by_(session, conditions)
+    db_team = team_ops.read_team_by_(session, conditions).first()
 
     if db_team is None:
         raise HTTPException(
-            status_code=404,
+            status.HTTP_404_NOT_FOUND,
             detail="Team not found"
         )
 
