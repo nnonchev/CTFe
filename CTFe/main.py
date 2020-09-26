2853aa2c742a73179d7d45470ea99cfb5aa5393d
nnonchev <nnonchev@protonmail.com>
2020-09-26 10:57:04 +0200

    refactoring

--- CTFe/main.py
+++ CTFe/main.py
@@ -4,7 +4,10 @@ from fastapi import (
 )
 
 from CTFe.config.database import dal
-from CTFe.utils import validators
+from CTFe.utils import (
+    validators,
+    enums,
+)
 from CTFe.views import (
     auth_router,
     user_router,
@@ -24,35 +27,35 @@ app.include_router(
     user_router,
     prefix="/users",
     dependencies=[
-        Depends(validators.validate_admin),
+        Depends(validators.validate_user_type(enums.UserType.ADMIN)),
     ],
 )
 app.include_router(
     team_router,
     prefix="/teams",
     dependencies=[
-        Depends(validators.validate_admin),
+        Depends(validators.validate_user_type(enums.UserType.ADMIN)),
     ],
 )
 app.include_router(
     challenge_router,
     prefix="/challenges",
     dependencies=[
-        Depends(validators.validate_admin),
+        Depends(validators.validate_user_type(enums.UserType.ADMIN)),
     ],
 )
 app.include_router(
     attempt_router,
     prefix="/attempts",
     dependencies=[
-        Depends(validators.validate_admin),
+        Depends(validators.validate_user_type(enums.UserType.ADMIN)),
     ],
 )
 app.include_router(
     player_router,
     prefix="/players",
     dependencies=[
-        Depends(validators.validate_player),
+        Depends(validators.validate_user_type(enums.UserType.PLAYER)),
     ],
 )
 
