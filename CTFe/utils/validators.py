2159d3a8988a7703c171af00d5c8237fb8951cf8
nnonchev <nnonchev@protonmail.com>
2020-09-26 10:56:35 +0200

    refactor validator for user_type

--- CTFe/utils/validators.py
+++ CTFe/utils/validators.py
@@ -9,30 +9,19 @@ from CTFe.operations import auth_ops
 from CTFe.utils import enums
 
 
-def _validate_user_type(
-    user_type: str,
-    db_user: User = Depends(auth_ops.get_current_user),
-):
-    if not enums.UserType.has_type(user_type):
-        raise ValueError("Wrong user type")
+class validate_user_type:
+    def __init__(self, user_type: str):
+        if not enums.UserType.has_type(user_type):
+            raise ValueError("Wrong user type")
 
-    if db_user.user_type != enums.UserType.ADMIN:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="You don't have the correct access"
-        )
+        self.user_type = user_type
 
-
-def validate_admin(): return (
-    _validate_user_type(enums.UserType.ADMIN)
-)
-
-
-def validate_player(): return (
-    _validate_user_type(enums.UserType.PLAYER)
-)
-
-
-def validate_contributor(): return (
-    _validate_user_type(enums.UserType.CONTRIBUTOR)
-)
+    async def __call__(
+        self,
+        db_user: User = Depends(auth_ops.get_current_user),
+    ):
+        if db_user.user_type != self.user_type:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="You don't have the correct access"
+            )
