fc7b5f10648a4492d95998d9ee5450b163df3ff9
nnonchev <nnonchev@protonmail.com>
2020-09-27 15:29:22 +0200

    added crud operations for contributors challenge

--- CTFe/views/contributor_view.py
+++ CTFe/views/contributor_view.py
@@ -13,12 +13,37 @@ from CTFe.config.database import dal
 from CTFe.operations import (
     contributor_ops,
     auth_ops,
+    challenge_ops,
 )
-from CTFe.models import User
-from CTFe.schemas import contributor_schemas
+from CTFe.models import (
+    User,
+    Challenge,
+)
+from CTFe.schemas import (
+    contributor_schemas,
+    challenge_schemas,
+)
 
 
 router = APIRouter()
+
+
+@router.get("/list-challenges", response_model=List[challenge_schemas.Details])
+def list_challenges(
+    *,
+    db_contributor: User = Depends(auth_ops.get_current_user),
+    session: Session = Depends(dal.get_session),
+) -> challenge_schemas.Details:
+    """ List all challenges created by the current contributor """
+
+    conditions = and_(
+        Challenge.owner_id == db_contributor.id,
+    )
+
+    db_challenges = challenge_ops.query_challenges_by_(
+        session, conditions).all()
+
+    return db_challenges
 
 
 @router.get("/username/{username}", response_model=contributor_schemas.Details)
@@ -105,3 +130,90 @@ async def delete_contributor(
     """ Delete contributor record from DB """
 
     contributor_ops.delete_contributor(session, db_contributor)
+
+
+@router.post("/create-challenge", response_model=contributor_schemas.Details)
+def create_challenge(
+    *,
+    challenge_create: challenge_schemas.Create,
+    db_contributor: User = Depends(auth_ops.get_current_user),
+    session: Session = Depends(dal.get_session),
+) -> contributor_schemas.Details:
+    """ Create challenge and assign this contributor to it """
+
+    from CTFe.operations import challenge_ops
+
+    # Challenge name is already taken
+    conditions = and_(
+        Challenge.name == challenge_create.name,
+    )
+
+    db_challenge = challenge_ops.query_challenges_by_(
+        session, conditions).first()
+
+    if db_challenge is not None:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"The name: { challenge_create.name } is already taken",
+        )
+
+    contributor_ops.create_challenge(
+        session, challenge_create, db_contributor)
+
+    return db_contributor
+
+
+@router.put("/update-challenge/{challenge_id}", response_model=contributor_schemas.Details)
+def update_challenge(
+    *,
+    challenge_id: int,
+    challenge_update: challenge_schemas.Update,
+    db_contributor: User = Depends(auth_ops.get_current_user),
+    session: Session = Depends(dal.get_session),
+) -> contributor_schemas.Details:
+    """ Update contributor's challenge """
+
+    conditions = and_(
+        Challenge.id == challenge_id,
+        Challenge.owner_id == db_contributor.id,
+    )
+
+    db_challenge = challenge_ops.query_challenges_by_(session, conditions).first()
+
+    if db_challenge is None:
+        raise HTTPException(
+            status.HTTP_404_NOT_FOUND,
+            detail="Challenge not found",
+        )
+        
+    challenge_ops.update_challenge(session, db_challenge, challenge_update)
+
+    return db_contributor
+
+
+@router.post("/remove-challenge/{challenge_id}", response_model=contributor_schemas.Details)
+def remove_challenge(
+    *,
+    challenge_id: int,
+    db_contributor: User = Depends(auth_ops.get_current_user),
+    session: Session = Depends(dal.get_session),
+) -> contributor_schemas.Details:
+    """ Delete a challenge related to this contributor """
+
+    conditions = and_(
+        Challenge.id == challenge_id,
+        Challenge.owner_id == db_contributor.id,
+    )
+
+    db_challenge = challenge_ops.query_challenges_by_(
+        session, conditions).first()
+
+    if db_challenge is None:
+        raise HTTPException(
+            status.HTTP_404_NOT_FOUND,
+            detail="Challenge not found",
+        )
+
+    challenge_ops.delete_challenge(session, db_challenge)
+
+    return db_contributor
